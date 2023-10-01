bf7716858107e0d560e4ce0c9319fc35e1f76943
malkybar <40530828@live.napier.ac.uk>
2023-10-01 18:03:28 +0100

    chonks

--- chonky.py
+++ chonky.py
@@ -35,5 +35,12 @@ def page_not_found(error):
 def force404():
     abort(404)
     
+@app.route('/static-example/img')
+def static_example_img():
+    start = '<img src="'
+    url = url_for('static', filename='vmask.jpg')
+    end = '">'
+    return start+url+end, 200
+    
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
